0cff747d9a583ee0039e22afb0528f962972abb2
기범 <lcparkgb@gmail.com>
2026-04-04 22:57:33 +0900

    Add files via upload

---
+++ chapter01.js
@@ -0,0 +1,24 @@
+// 1. 변수:변하는 값
+let age;
+
+age = 30;
+
+// 2. 상수:변할수 없는 값
+const brith = "1997.01.07";
+
+// 3. 변수 명명규칙(네이밍 규칙)
+// 3-1. $, _ 제외한 기호는 사용할 수 없다.
+let $_name;
+
+// 3-2. 숫자로 시작할 수 없다.
+let name1;
+let _2name;
+
+// 3-3. 예약어를 사용할 수 없다.
+// let let (x)
+// let if (x)
+
+// 4. 변수 명명 추천
+let salesCount = 1;
+let refundCount = 1;
+let totalSalesCount = salesCount - refundCount
