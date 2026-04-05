3125da73da6ffdb64bb8b4aaf2dc98e63b75deb2
기범 <lcparkgb@gmail.com>
2026-04-05 17:49:30 +0900

    Add files via upload

---
+++ chapter02.js
@@ -0,0 +1,31 @@
+// 1. Number Type
+let num1 = 27;
+let num2 = 1.5;
+let num3 = -20;
+
+let inf = Infinity;
+let mInf = -Infinity;
+
+let nan = NaN;
+
+console.log(1 * "hello");
+
+// 2. String Type
+let myName = "park"
+let myLocation = "incheon"
+let introduce = myName + myLocation
+
+let introduceText = `${myName}은 ${myLocation}에 거주합니다`;
+
+console.log(introduceText)
+
+// 3. Boolean Type 
+let isSwitchOn = true;
+let isEmpty = false;
+
+// 4. Null Type (아무것도 없다)
+let empty = null;
+
+// 5. Undefined Type 
+let none;
+console.log(none);
